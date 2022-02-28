33ce11e1c8c8688c1afac49b8b71f3e3661c1ce6
wangzl1163 <wangzl1163@qq.com>
2022-02-28 14:57:15 +0800

    feat：开发环境API代理模板

--- vite.config.ts
+++ vite.config.ts
@@ -27,5 +27,38 @@ export default defineConfig({
 			iconDirs: [path.resolve(__dirname, 'src/Assets/Icons/svg')],
 			symbolId: 'icon-[dir]-[name]'
 		})
-	]
+	],
+	server: {
+		port: 3000,
+		strictPort: true,
+		proxy: {
+			// 字符串简写写法
+			'/foo': 'http://localhost:4567',
+			// 选项写法
+			'/api': {
+			  target: 'http://jsonplaceholder.typicode.com',
+			  changeOrigin: true,
+			  rewrite: (path) => path.replace(/^\/api/, '')
+			},
+			// 正则表达式写法
+			'^/fallback/.*': {
+			  target: 'http://jsonplaceholder.typicode.com',
+			  changeOrigin: true,
+			  rewrite: (path) => path.replace(/^\/fallback/, '')
+			},
+			// 使用 proxy 实例
+			'/api': {
+			  target: 'http://jsonplaceholder.typicode.com',
+			  changeOrigin: true,
+			  configure: (proxy, options) => {
+				 // proxy 是 'http-proxy' 的实例
+			  }
+			},
+			// Proxying websockets or socket.io
+			'/socket.io': {
+			  target: 'ws://localhost:3000',
+			  ws: true
+			}
+		}
+	}
 })
