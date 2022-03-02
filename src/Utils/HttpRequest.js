26f1a28285b010c18bd7b3192f67956e68c06c52
wangzl1163 <wangzl1163@qq.com>
2022-03-02 11:29:35 +0800

    fix：修复条件判断错误

--- src/Utils/HttpRequest.js
+++ src/Utils/HttpRequest.js
@@ -8,7 +8,7 @@ let queue401 = []
 
 function getCookie(name) {
    var arr, reg = new RegExp("(^| )" + name + "=([^;]*)(;|$)");
-   if (arr = document.cookie.match(reg)) return unescape(arr[2]);
+   if (arr === document.cookie.match(reg)) return unescape(arr[2]);
    else return null;
 }
 
