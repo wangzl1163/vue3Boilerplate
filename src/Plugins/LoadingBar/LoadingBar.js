f63e09136942803a26f7aeead2a34deaad6529e6
wangzl1163 <wangzl1163@qq.com>
2022-03-08 17:26:33 +0800

    fix：修复 loading bar 不显示

--- src/Plugins/LoadingBar/LoadingBar.js
+++ src/Plugins/LoadingBar/LoadingBar.js
@@ -5,36 +5,41 @@
  * @LastEditTime : 2022-02-23 16:53:19
  * @LastEditors  : 王占领
  */
-import LoadingBar from './LoadingBar.vue'
-import {createApp, h} from 'vue'
+import LoadingBar from "./LoadingBar.vue";
+import { createVNode, render } from "vue";
 
-LoadingBar.newInstance = properties => {
-   const _props = () => properties || {}
-   const container = document.createElement('div')
-   const Instance = createVNode(LoadingBar, _props)
+LoadingBar.newInstance = (properties) => {
+   const _props = (() => properties || {})();
+   const container = document.createElement("div");
+   const Instance = createVNode(LoadingBar, _props);
 
-   render(Instance, container)
-   document.body.appendChild(container.firstElementChild)
-   
-   const loading_bar = Instance.component
+   render(Instance, container);
+   document.body.appendChild(container.firstElementChild);
+
+   const loading_bar = Instance.component;
 
    return {
-      update (options) {
-         if ('percent' in options) {
-            loading_bar.percent = options.percent
+      update(options) {
+         if ("percent" in options) {
+            loading_bar.proxy.percent = options.percent;
          }
          if (options.status) {
-            loading_bar.status = options.status
+            loading_bar.proxy.status = options.status;
          }
-         if ('show' in options) {
-            loading_bar.show = options.show
+         if ("show" in options) {
+            loading_bar.proxy.show = options.show;
          }
       },
       component: loading_bar,
-      destroy () {
-         document.body.removeChild(document.getElementsByClassName('loading-bar')[0])
+      destroy() {
+         Instance.props.onDestroy = () => {
+            render(null, container);
+         };
+         document.body.removeChild(
+            document.getElementsByClassName("loading-bar")[0]
+         );
       }
-   }
-}
+   };
+};
 
-export default LoadingBar
+export default LoadingBar;
