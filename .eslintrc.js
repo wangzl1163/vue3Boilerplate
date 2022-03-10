4261fec877501d2a38828022dfd60f8bb8ac0944
wangzl1163 <wangzl1163@qq.com>
2022-03-10 14:06:32 +0800

    feat：配置 vite eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,27 +7,28 @@
  */
 
 module.exports = {
-	env: {
-		browser: true,
-		node: true,
-		es2022: true
-	},
-	extends: ['eslint:recommended', 'prettier'],
-	plugins: [],
-	parser: 'vue-eslint-parser',
-	parserOptions: {
-		sourceType: 'module',
-		ecmaVersion: 'latest',
-		ecmaFeatures: {
-			jsx: true
-		}
-	},
-	globals: {
-		// 指定全局变量，用以避免报no-undef错误
-		defineProps: 'writable',
-		defineEmits: 'writable'
-	},
-	rules: {
-		'no-tabs': ['error', { allowIndentationTabs: true }]
-	}
-}
+   env: {
+      browser: true,
+      node: true,
+      es2022: true
+   },
+   extends: ["eslint:recommended", "prettier"],
+   plugins: [],
+   parser: "vue-eslint-parser",
+   parserOptions: {
+      sourceType: "module",
+      ecmaVersion: "latest",
+      ecmaFeatures: {
+         jsx: true
+      }
+   },
+   globals: {
+      // 指定全局变量，用以避免报no-undef错误
+      defineProps: "writable",
+      defineEmits: "writable"
+   },
+   rules: {
+      "no-tabs": ["error", { allowIndentationTabs: true }],
+      "no-unused-vars": ["off"]
+   }
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,64 +1,64 @@
-/*
- * @Description  : vite config
- * @Author       : 王占领
- * @Date         : 2022-02-23 10:33:12
- * @LastEditTime : 2022-02-24 15:43:50
- * @LastEditors  : 王占领
- */
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
+import eslintPlugin from "@nabla/vite-plugin-eslint";
 
-const path = require('path')
+const path = require("path");
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, './src')
-		}
-	},
-	plugins: [
-		vue(),
-		vueJsx({}),
-		createSvgIconsPlugin({
-			// 配置路劲在你的src里的svg存放文件
-			iconDirs: [path.resolve(__dirname, 'src/Assets/Icons/svg')],
-			symbolId: 'icon-[dir]-[name]'
-		})
-	],
-	server: {
-		port: 3000,
-		strictPort: true,
-		proxy: {
-			// 字符串简写写法
-			'/foo': 'http://localhost:4567',
-			// 选项写法
-			'/api': {
-			  target: 'http://jsonplaceholder.typicode.com',
-			  changeOrigin: true,
-			  rewrite: (path) => path.replace(/^\/api/, '')
-			},
-			// 正则表达式写法
-			'^/fallback/.*': {
-			  target: 'http://jsonplaceholder.typicode.com',
-			  changeOrigin: true,
-			  rewrite: (path) => path.replace(/^\/fallback/, '')
-			},
-			// 使用 proxy 实例
-			'/api': {
-			  target: 'http://jsonplaceholder.typicode.com',
-			  changeOrigin: true,
-			  configure: (proxy, options) => {
-				 // proxy 是 'http-proxy' 的实例
-			  }
-			},
-			// Proxying websockets or socket.io
-			'/socket.io': {
-			  target: 'ws://localhost:3000',
-			  ws: true
-			}
-		}
-	}
-})
+   resolve: {
+      alias: {
+         "@": path.resolve(__dirname, "./src")
+      }
+   },
+   plugins: [
+      vue(),
+      vueJsx({}),
+      createSvgIconsPlugin({
+         // 配置路劲在你的src里的svg存放文件
+         iconDirs: [path.resolve(__dirname, "src/Assets/Icons/svg")],
+         symbolId: "icon-[dir]-[name]"
+      }),
+      eslintPlugin({
+         eslintOptions: {
+            fix: true,
+            cache: false
+         }
+      })
+   ],
+   server: {
+      port: 3000,
+      strictPort: true,
+      proxy: {
+         // 字符串简写写法
+         "/foo": "http://localhost:4567",
+         // 选项写法
+         "/api": {
+            target: "http://jsonplaceholder.typicode.com",
+            changeOrigin: true,
+            rewrite: (path) => path.replace(/^\/api/, "")
+         },
+         // 正则表达式写法
+         "^/fallback/.*": {
+            target: "http://jsonplaceholder.typicode.com",
+            changeOrigin: true,
+            rewrite: (path) => path.replace(/^\/fallback/, "")
+         },
+         // 使用 proxy 实例
+         "/api": {
+            target: "http://jsonplaceholder.typicode.com",
+            changeOrigin: true,
+            configure: (proxy, options) => {
+               // proxy 是 'http-proxy' 的实例
+            }
+         },
+         // Proxying websockets or socket.io
+         "/socket.io": {
+            target: "ws://localhost:3000",
+            ws: true
+         }
+      }
+   }
+});
