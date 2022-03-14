7bb819a2ab760ebd9c2492a771681c382f75ea16
wangzl1163 <wangzl1163@qq.com>
2022-03-14 17:23:20 +0800

    refactor：重构声明

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,15 +1,12 @@
-/*
- * @Description: 
- * @Author: 王占领
- * @Date: 2022-02-18 09:52:06
- * @LastEditTime: 2022-02-18 15:58:52
- * @LastEditors: 王占领
- */
 /// <reference types="vite/client" />
 
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
-  const component: DefineComponent<{}, {}, any>
-  export default component
+declare module "*.vue" {
+   import type { DefineComponent } from "vue";
+
+   const component: DefineComponent<{}, {}, any>;
+   export default component;
 }
+
+// 定义 module 防止 ts 编译报错
+declare module "big-element";
+declare module "big-element/packages/Components/Echarts";
