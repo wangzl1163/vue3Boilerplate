f6a90b086661781d760e79e4db78e4dd9d12f892
wangzl1163 <wangzl1163@qq.com>
2022-04-15 17:16:48 +0800

    perf：优化展示

--- src/Settings.js
+++ src/Settings.js
@@ -1,18 +1,18 @@
-const title = '大数据监控运维平台'
+const title = "****平台";
 
 export default {
-	title,
-	titleSuffix: '-' + title,
-	/**
-	 * 是否显示 tagsView
-	 */
-	tagsView: true,
-	/**
-	 * 是否固定头部
-	 */
-	fixedHeader: false,
-	// 加密 key
-	aesKey: '',
-	// 加密 iv
-	aesIv: ''
-}
+   title,
+   titleSuffix: "-" + title,
+   /**
+    * 是否显示 tagsView
+    */
+   tagsView: true,
+   /**
+    * 是否固定头部
+    */
+   fixedHeader: false,
+   // 加密 key
+   aesKey: "",
+   // 加密 iv
+   aesIv: ""
+};
