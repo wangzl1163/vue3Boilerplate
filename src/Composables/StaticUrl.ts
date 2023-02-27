723d3a50da475fa58ada0dd533ffdfda07290f27
wangzl1163 <wangzl1163@qq.com>
2023-02-27 11:35:19 +0800

    refactor：重构 staticUrl

--- src/Composables/StaticUrl.ts
+++ src/Composables/StaticUrl.ts
@@ -2,20 +2,17 @@
  * @Description: 生成静态资源的 url
  * @Author: 王占领
  * @Date: 2023-02-13 16:26:55
- * @LastEditTime: 2023-02-13 16:33:48
+ * @LastEditTime: 2023-02-23 11:07:55
  * @LastEditors: 王占领
  */
 export function useStaticUrl() {
    return (
       name: string,
-      option: { path: string; type: "png" | "gif" } = {
-         path: "Images",
-         type: "png"
-      }
+      {
+         path = "Images",
+         type = "png"
+      }: { path?: string; type?: "png" | "gif" | "mp4" } = {}
    ) => {
-      return new URL(
-         `../Assets/${option.path}/${name}.${option.type}`,
-         import.meta.url
-      ).href;
+      return new URL(`../Assets/${path}/${name}.${type}`, import.meta.url).href;
    };
 }
