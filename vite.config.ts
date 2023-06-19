cc8f7742ac28bc2223bd20e99604fa31d94f251a
wangzl1163 <wangzl1163@qq.com>
2023-06-19 15:34:01 +0800

    fix: 重装依赖修复构建时报错

--- vite.config.ts
+++ vite.config.ts
@@ -36,7 +36,7 @@ const proxyRequestLog = (
    console.groupEnd();
 };
 
-// https://vitejs.dev/config/
+// https://cn.vitejs.dev/config/
 export default defineConfig({
    resolve: {
       alias: {
